1d437e721cc456b3996d5103ca11fd3b595449a7
Lukas Neicelis <l.neicelis@gmail.com>
2015-05-18 22:47:37 +0300

    Using generator fn in db preparation

--- test/prepare.js
+++ test/prepare.js
@@ -1,29 +1,26 @@
 /* global require */
 
 var config = require('../app/config');
+var co = require('co');
 var r = require('rethinkdbdash')(config.rethinkdb);
 
-r.dbList().run().then(function (array) {
-    if (array.indexOf(config.rethinkdb.db) > -1) {
-        console.log('Dropping database');
-        r.dbDrop(config.rethinkdb.db).run();
+co(function* () {
+    var databases = yield r.dbList().run();
+    if (databases.indexOf(config.rethinkdb.db) > -1) {
+        console.log('Dropping database.');
+        yield r.dbDrop(config.rethinkdb.db).run();
     }
+    console.log('Creating database.');
+    yield r.dbCreate(config.rethinkdb.db).run();
 
-    var promise = Promise.all([
-        r.dbCreate(config.rethinkdb.db).run(),
-        r.db(config.rethinkdb.db).tableCreate('users').run(),
-        r.db(config.rethinkdb.db).tableCreate('groups').run(),
-        r.db(config.rethinkdb.db).tableCreate('groups_users').run()
-    ]);
-
-    promise.then(function () {
-        console.log('Tables successfully created');
-        process.exit(0);
-    }, function (err) {
-        console.log('Failed while creating tables', err);
-        process.exit(1);
-    });
+    console.log('Creating tables.');
+    yield r.db(config.rethinkdb.db).tableCreate('users').run();
+    yield r.db(config.rethinkdb.db).tableCreate('groups').run();
+    yield r.db(config.rethinkdb.db).tableCreate('groups_users').run();
+}).then(function () {
+    console.log('Database is prepared.');
+    process.exit(0);
 }, function (err) {
-    console.log('Failed to list databases.');
+    console.log('Failed to prepare database.');
     process.exit(1);
 });
