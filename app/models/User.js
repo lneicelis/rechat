e31d14b1be8e439e13ba2747219ee6c69267a1e3
Lukas Neicelis <l.neicelis@gmail.com>
2015-07-25 22:47:49 +0300

    Adding method to retrieve all user groups

--- app/models/User.js
+++ app/models/User.js
@@ -1,4 +1,6 @@
 var Model = require('./Model');
+var config = require('../config');
+var r = require('rethinkdbdash')(config.rethinkdb);
 
 var User = function (data) {
     this.describe({
@@ -11,5 +13,13 @@ var User = function (data) {
 
 User.prototype = Object.create(Model.prototype);
 User.prototype.constructor = User;
+
+User.prototype.getGroups = function () {
+    var query = r.table("groups_users")
+        .filter({userId: this.id})
+        .eqJoin('groupId', r.table("groups"));
+
+    return this.fetchJoin(query, User);
+};
 
 module.exports = User;
